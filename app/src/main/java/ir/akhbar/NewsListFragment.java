ea31d8c0da82994cf4b05d94d1e7294fd917f3cc
kabiri <rshahi23@yahoo.com>
2020-01-30 15:16:05 +0330

    Update gradle

--- app/src/main/java/ir/akhbar/NewsListFragment.java
+++ app/src/main/java/ir/akhbar/NewsListFragment.java
@@ -2,10 +2,10 @@ package ir.akhbar;
 
 import android.os.Bundle;
 import android.os.Handler;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.LayoutInflater;
@@ -17,7 +17,6 @@ import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import retrofit2.Call;
 import retrofit2.Callback;
